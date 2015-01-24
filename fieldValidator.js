52c57e54717259d9bd469f9a628cb60b7e297945
Greg Barozzi <gregbarozzi@gmail.com>
2015-01-24 10:25:13 -0800

    include source for fieldValidator.js

---
+++ fieldValidator.js
@@ -0,0 +1,52 @@
+var validate = FieldValidator();
+var hasErrors = 0;
+
+if (targetEntity.getQualifiedAttribute("customAttributes.transgenicAnimals.customAttributes.creatingATransgenicRodent") === true) {
+    hasErrors += validate.field("customAttributes.transgenicAnimals.customAttributes.tAProtocol").isNotNull();
+}
+hasErrors += validate.field("customAttributes.descriptionOfProcedures").isNotNull();
+
+if(hasErrors) throw new Error(-1,"There are " + hasErrors + " missing field(s). Please see below.");
+
+/*
+ * Field Vaidator
+ * Written by: Greg Barozzi
+ * Field Validator returns a module-like object that simplifies 
+ * custom validation in Click Portal views. Due to the 'Free code'
+ * restriction this source must be included in the validation script
+ * hook for each view that requires custom validation.
+ */
+function FieldValidator() {
+    var validator = {
+        property: "",
+        entity: null,
+        field: function(property) {
+            this.property = property;
+            this.entity = targetEntity.getQualifiedAttribute(property);
+            return this;
+        },
+        isNull: function() {
+            if (this.entity == null) return 0;
+            targetEntity.reportFieldError("_ClickAgreement." + this.property, "Please provide additional information.");
+            return 1;
+        },
+        isNotNull: function() {
+            if (this.entity != null) return 0;
+            targetEntity.reportFieldError("_ClickAgreement." + this.property, "Please provide additional information.");
+            return 1;
+        },
+        matchesEntity: function(entity) {
+            if (this.entity === entity) return 0;
+            targetEntity.reportFieldError("_ClickAgreement." + this.property, "Please provide additional information.");
+            return 1;
+        },
+        setCountNotZero: function() {
+            if (this.entity && this.entity.count() > 0) return 0;
+            targetEntity.reportFieldError("_ClickAgreement." + this.property, "Please provide additional information.");
+            return 1;
+        }
+    };
+    return validator;
+}
+
+
